2f96f0d72a97f9f9f8ae2b0988fd5170855093c7
FurkanCan <furkancan1342@gmail.com>
2023-05-01 17:28:04 +0300

    exele kayıt etme

--- bitcoinVeriCekme.py
+++ bitcoinVeriCekme.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from time import sleep
+import openpyxl
 
 
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
 
 driver = webdriver.Chrome("/Applications")
 driver.maximize_window()
@@ -10,6 +13,7 @@ driver.delete_all_cookies()
 driver.get("https://tr.tradingview.com/chart/?symbol=BITSTAMP%3ABTCUSD")
 driver.implicitly_wait(10)
 while True:
-    fiyat_Bilgisi = driver.find_element("xpath", "/html/body/div[2]/div[6]/div/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[2]/span[1]/span[1]").text
-    print(fiyat_Bilgisi)
+    BTC_fiyat = driver.find_element("xpath", "/html/body/div[2]/div[6]/div/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[2]/span[1]/span[1]")
+    worksheet.append([BTC_fiyat.text ])
+    workbook.save('BTC.xlsx')
     sleep(10)
